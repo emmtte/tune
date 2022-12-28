7628a5a32d0a699fed605f5e0860d7dd81952ca3
emmtte <1827696+emmtte@users.noreply.github.com>
2022-12-28 12:44:07 +0100

    Update sst.mjs

--- sst.mjs
+++ sst.mjs
@@ -3,19 +3,13 @@ const chatId=
 
 import axios from 'axios'
 import { Bot } from 'grammy' ; const bot = new Bot(token)
-import parser from 'fast-xml-parser'
+import { XMLParser } from 'fast-xml-parser' ; const parser = new XMLParser();
 import { EventEmitter } from 'events' ; const event = new EventEmitter()
-import fns from 'date-fns' ; const { add, addSeconds, addMinutes, parseISO, getUnixTime, format} = fns
-import fns_tz from 'date-fns-tz' ; const { zonedTimeToUtc } = fns_tz
-import schedule from 'node-schedule'
-const globalTimeZone = 'America/New_York'
-import {decode} from 'html-entities'
-import urlExist from "url-exist"
-let job
+import fns from 'date-fns' ; const { format, getTime, parseISO } = fns
+import { decode } from 'html-entities'
+import { schedule } from 'node-cron'
 
-schedule.scheduleJob('0 * * * * *', function(){console.log(format(Date.now(),"yyyy-MM-dd HH:mm:ss"))});
-
-bot.start()
+schedule('0 * * * * *'       , function() { console.log( format( Date.now(), "yyyy-MM-dd HH:mm:ss" ) ) } )
 
 const url = 'http://www.streamingsoundtracks.com/soap/FM24seven.php';
 const xmls = `<?xml version="1.0" encoding="ISO-8859-1" standalone="no" ?>
@@ -31,38 +25,51 @@ const xmls = `<?xml version="1.0" encoding="ISO-8859-1" standalone="no" ?>
  </SOAP-ENV:Body>
 </SOAP-ENV:Envelope>`
 
+event.on('wait', async () => {
+  await new Promise(resolve => setTimeout(resolve, 60000))
+  event.emit('xml')
+} )
+
 event.on('xml', () => {
 axios.post(url, xmls, {headers: {'Content-Type': 'text/xml'} })
-  .then(res=>{
+  .then( res => {
   let obj = parser.parse(res.data)['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ns1:GetCurrentlyPlayingResponse']['Result']
-  console.log(obj,'\n')
-  event.emit('tm',obj) })
-  .catch(err=>{
-    console.log(err)
-    job = schedule.scheduleJob(addMinutes(Date.now(),1), () => { job.cancel(); event.emit('xml')})
-  })
+  console.log( '\n', obj, '\n' )
+  event.emit( 'tm', obj ) })
+  .catch( err => { console.log(err) ; event.emit('wait') } )
 })
 
-event.on('tm', async (obj) => {
-let now = zonedTimeToUtc(parseISO(obj.SystemTime),globalTimeZone)
-let end = zonedTimeToUtc(add(parseISO(obj.PlayStart), { seconds: getUnixTime(obj.Length)+1}),globalTimeZone) //add 1s because of 100eme
-let time  = addSeconds(new Date(0), getUnixTime(obj.Length)) ; let length = format(time, 'm:ss');
-console.log('now:',now) ; console.log('end:',end)
-let cover=obj.CoverLink.replace('cover','cover/500')
+event.on('tm', async ( { Album, Track, Artist, Length, PlayStart, SystemTime, CoverLink } ) => {
+  SystemTime = parseISO( SystemTime ) ; PlayStart = parseISO( PlayStart )
+  let waitingTime = getTime( PlayStart ) + Length - getTime( SystemTime )
+  console.log('PlayStart   :', PlayStart, ' : ', getTime(PlayStart))
+  console.log('SystemTime  :', SystemTime, ' : ', getTime(SystemTime))
+  console.log('Lenght      :', format(Length, 'mm:ss'), ' : ', Length )
+  console.log('WaitingTime :', format( waitingTime, 'mm:ss' ), ' : ', waitingTime )
+  if (waitingTime<0) {waitingTime=15000 ; console.log('WaitingTime :', format( waitingTime, 'mm:ss' ), ' : ', waitingTime ) }
 
-let album, newAlbum
-let oldAlbum = decode(obj.Album)
-newAlbum = oldAlbum.toString().replace(', The','')
-if (oldAlbum == newAlbum){
-  newAlbum = oldAlbum.toString().replace(', A','')
-  if (oldAlbum == newAlbum) { album = oldAlbum 
-  } else {album = 'A ' + newAlbum}
-} else {album = 'The ' + newAlbum} 
- console.log(oldAlbum,'==',newAlbum,'=>',album)
+  let Cover=CoverLink.replace('cover','cover/500')
 
-let caption= `<b>${album}</b>\n<i>${decode(obj.Artist)}</i>\n${decode(obj.Track)} [${length}]`
-if ((await urlExist(cover)) && (obj.Album !== 'StationID')) {bot.api.sendPhoto(chatId, cover, {caption:caption, parse_mode:'html'})}
-job = schedule.scheduleJob(end, () => { job.cancel(); event.emit('xml')})
+  let NewAlbum
+  let OldAlbum = decode(Album)
+  NewAlbum = OldAlbum.toString().replace(', The','')
+  if (OldAlbum == NewAlbum) { NewAlbum = OldAlbum.toString().replace(', A','')
+    if (OldAlbum == NewAlbum) { Album = OldAlbum } else {Album = 'A ' + NewAlbum }
+  } else {Album = 'The ' + NewAlbum}
+
+  console.log()
+  console.log( OldAlbum, ' == ', NewAlbum, ' => ', Album )
+  console.log()
+
+  let caption= `<b>${Album}</b>\n<i>${decode(Artist)}</i>\n${decode(Track)} [${format(Length, 'm:ss')}]`
+  if (await axios( Cover ).then( () => true ).catch( () => false ) && Album !== 'StationID') {
+    console.log(Cover)
+    await bot.api.sendPhoto( chatId, Cover, { caption, parse_mode:'html'} ).catch( err => console.log( err ) )
+  }
+
+  await new Promise(resolve => setTimeout(resolve, waitingTime))
+  event.emit('xml')
+
 })
 
 event.emit('xml')
